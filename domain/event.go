029861856281d2803078db9f4be4c8a904acec69
violetaplum <violetaplume@gmail.com>
2025-01-09 15:45:37 +0900

    #3 PostgreSQL 구현체 작성

--- domain/event.go
+++ domain/event.go
@@ -34,11 +34,12 @@ type MoneyWithdrawnEvent struct {
 	Timestamp time.Time
 }
 
+// AccountCreatedEvent implements Event
 func (e AccountCreatedEvent) GetType() EventType {
 	return AccountCreated
 }
 
-func (e AccountCreatedEvent) GetAggregatedId() string {
+func (e AccountCreatedEvent) GetAggregateId() string {
 	return e.AccountId
 }
 
@@ -47,5 +48,39 @@ func (e AccountCreatedEvent) GetData() interface{} {
 }
 
 func (e AccountCreatedEvent) GetTimestamp() time.Time {
+	return e.Timestamp
+}
+
+// MoneyDepositedEvent implements Event
+func (e MoneyDepositedEvent) GetType() EventType {
+	return MoneyDeposited
+}
+
+func (e MoneyDepositedEvent) GetAggregateId() string {
+	return e.AccountId
+}
+
+func (e MoneyDepositedEvent) GetData() interface{} {
+	return e
+}
+
+func (e MoneyDepositedEvent) GetTimestamp() time.Time {
+	return e.Timestamp
+}
+
+// MoneyWithdrawnEvent implements Event
+func (e MoneyWithdrawnEvent) GetType() EventType {
+	return MoneyWithdrawn
+}
+
+func (e MoneyWithdrawnEvent) GetAggregateId() string {
+	return e.AccountId
+}
+
+func (e MoneyWithdrawnEvent) GetData() interface{} {
+	return e
+}
+
+func (e MoneyWithdrawnEvent) GetTimestamp() time.Time {
 	return e.Timestamp
 }
--- repository/postgres/event_store.go
+++ repository/postgres/event_store.go
@@ -1,7 +1,11 @@
 package postgres
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"go-eventsourcing-patterns/domain"
 )
 
 type EventStore struct {
@@ -10,4 +14,32 @@ type EventStore struct {
 
 func NewEventStore(db *sql.DB) *EventStore {
 	return &EventStore{db: db}
+}
+
+func (s *EventStore) Save(ctx context.Context, accountId string, events []domain.Event) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("error beginning transaction: %w", err)
+	}
+
+	defer tx.Rollback()
+
+	query := `INSERT INTO events (aggregate_id, event_type, event_data, created_at) 
+				VALUES ($1, $2, $3, $4)`
+
+	for _, event := range events {
+		eventData, err := json.Marshal(event.GetData())
+		if err != nil {
+			return fmt.Errorf("error marshaling event: %w", err)
+		}
+
+		_, err = tx.ExecContext(ctx, query, accountId, string(event.GetType()),
+			eventData, event.GetTimestamp())
+
+		if err != nil {
+			return fmt.Errorf("error inserting event: %w", err)
+		}
+	}
+
+	return tx.Commit()
 }
